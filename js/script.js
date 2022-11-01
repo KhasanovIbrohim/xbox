1dbbf0b06ed73b74ab83c72936412fdf55714390
IbrohimKhasanov <ibrohimdev01@gmail.com>
2022-11-01 22:28:16 +0500

    2.5

--- js/script.js
+++ js/script.js
@@ -67,3 +67,21 @@ function closeAll() {
     openBtn.style.display = "block";
     hideBtn.style.display = "none";
 }
+
+function reveal() {
+    var reveals = document.querySelectorAll(".reveal");
+
+    for (var i = 0; i < reveals.length; i++) {
+        var windowHeight = window.innerHeight;
+        var elementTop = reveals[i].getBoundingClientRect().top;
+        var elementVisible = 150;
+
+        if (elementTop < windowHeight - elementVisible) {
+            reveals[i].classList.add("active");
+        } else {
+            reveals[i].classList.remove("active");
+        }
+    }
+}
+
+window.addEventListener("scroll", reveal);
